26b903000525ad764f81eb682e63934bd9acf0c8
zhangchaoyang <18638062377@163.com>
2018-05-28 18:23:33 +0800

    测试

--- src/main/java/com/tthome/visneymanager/entity/User.java
+++ src/main/java/com/tthome/visneymanager/entity/User.java
@@ -2,7 +2,8 @@ package com.tthome.visneymanager.entity;
 
 public class User {
     public static void main(String[] args) {
-        System.out.printf("分支");
 
+        System.out.printf("分支");
+        System.out.printf("分支222");
     }
 }
