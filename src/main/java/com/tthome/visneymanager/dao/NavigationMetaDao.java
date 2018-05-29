1a6d09e79477c35bdb3ac18e8067df2e7b8cc709
mengxiangtao <774161192@qq.com>
2018-05-29 11:17:21 +0800

    修改了文件位置

---
+++ src/main/java/com/tthome/visneymanager/dao/NavigationMetaDao.java
@@ -0,0 +1,17 @@
+package com.tthome.visneymanager.dao;
+
+import com.tthome.visneymanager.entity.NavigationMeta;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author NBUG
+ * @date 2018/5/22 14:34
+ */
+public interface NavigationMetaDao {
+    public List<NavigationMeta> metaGetAll();
+    public int getMetaTotal();
+    public int metaAdd(NavigationMeta navigationMeta);
+
+}
