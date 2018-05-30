a0819779e61047ea09f26619c6643bd4f11faafc
zhangchaoyang <18638062377@163.com>
2018-05-30 19:39:28 +0800

    恢复版本并提交

--- src/main/java/com/tthome/visneymanager/utils/AuthFilter.java
+++ src/main/java/com/tthome/visneymanager/utils/AuthFilter.java
@@ -5,4 +5,7 @@ package com.tthome.visneymanager.utils;
  * @date 2018-05-29 16:26
  */
 public class AuthFilter {
+    public static void main(String[] args) {
+        System.out.printf("ssss");
+    }
 }
